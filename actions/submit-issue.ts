552b04a991b7d636e66b17e257dbc92228e3a483
you jung song <yuri3340@gmail.com>
2025-06-23 20:52:34 -0400

    ..

--- actions/submit-issue.ts
+++ actions/submit-issue.ts
@@ -27,8 +27,7 @@ export async function submitIssue(
 await prisma.issue.create({
   data: {
     id: crypto.randomUUID(),  // 직접 생성해도 되고
-    firstName: result.data.firstName,
-    lastName: result.data.lastName,
+    firstName: result.data.name,
     email: result.data.email,
     description: result.data.description,
     updatedAt: new Date(),
